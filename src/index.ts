7d6956fbd2115ae797d9561557d9f6dec46680ee
vivek <vivekdamar28@gmail.com>
2026-08-25 10:27:48 +0530

    fix: configure API for production hosting

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@
 
 const app = buildServer();
 
-app.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
+const port = Number(process.env.PORT || 3000);
+
+app.listen(
+  {
+    port,
+    host: "0.0.0.0",
+  },
+  (err, address) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+
+    console.log(`Job scheduler API running at ${address}`);
   }
-  console.log(`Job scheduler API running at ${address}`);
-});
+);
